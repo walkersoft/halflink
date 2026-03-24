258a580b54722bda2cf661a54abe3a15cd0e970d
Jason L. Walker <jasonlanewalker@gmail.com>
2026-03-24 14:05:17 -0500

    Setup cosmos settings

--- src/HalfLink.UI/Program.cs
+++ src/HalfLink.UI/Program.cs
@@ -1,5 +1,7 @@
-using MudBlazor.Services;
+using HalfLink.Core;
 using HalfLink.UI.Components;
+using Microsoft.Extensions.Options;
+using MudBlazor.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +11,9 @@ builder.Services.AddMudServices();
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+builder.Services.Configure<CosmosSettings>(builder.Configuration.GetSection(nameof(CosmosSettings)));
+builder.Services.AddSingleton(provider => provider.GetRequiredService<IOptions<CosmosSettings>>().Value);
 
 var app = builder.Build();
 
