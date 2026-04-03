2cf31464418be3e72ee7db9338ea6a1267b2aa33
Jason L. Walker <jasonlanewalker@gmail.com>
2026-04-03 11:43:25 -0500

    Added function to call endpoint in UI

---
+++ src/HalfLink.Functions/HalfLinkClickFunction.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Azure.Functions.Worker;
+using System.Net.Http.Json;
+
+namespace HalfLink.Functions
+{
+    internal class HalfLinkClickFunction(IHttpClientFactory httpClientFactory)
+    {
+        internal record ClickActivityEvent
+        {
+            public Guid LinkId { get; set; }
+            public string Referrer { get; set; } = string.Empty;
+            public DateTime ClickedAt { get; set; }
+        }
+
+        [Function(nameof(ReadHalfLinkClicks))]
+        public async Task ReadHalfLinkClicks([QueueTrigger("halflink-clicks", Connection = "AzureQueueStorage")] ClickActivityEvent message)
+        {
+            var httpClient = httpClientFactory.CreateClient("HalfLinkApiClient");
+            var response = await httpClient.PostAsJsonAsync("/s/click", message);
+            response.EnsureSuccessStatusCode();
+        }
+    }
+}
